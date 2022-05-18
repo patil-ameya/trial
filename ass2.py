c19b537173a6384f484afa4d4d67a14e2628094b
patil-ameya <72600064+patil-ameya@users.noreply.github.com>
2022-05-18 21:22:46 +0530

    Add files via upload

--- ass2.py
+++ ass2.py
@@ -1,30 +1,31 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-#reading Dataset
-dataset=pd.read_csv('D:\data.csv')
-X=dataset.iloc[:,:-1]
+dataset=pd.read_csv('data.csv')
+x=dataset.iloc[:,:-1]
 y=dataset.iloc[:,5]
 
 from sklearn.preprocessing import LabelEncoder
 le=LabelEncoder()
 
-X=X.apply(le.fit_transform)
-print("X")
+x=x.apply(le.fit_transform)
+print(x)
 
 from sklearn.tree import DecisionTreeClassifier
-regressor=DecisionTreeClassifier()
-regressor.fit(X.iloc[:,1:5],y)
 
-#Predict value for the given Expression
-X_in=np.array([1,1,0,0])
-y_pred=regressor.predict([X_in])
-print("Prediction:", y_pred)
+regressor=DecisionTreeClassifier(criterion='entropy')
+regressor.fit(x.iloc[:, 1:5],y)
+
+x_in=np.array([1,1,0,0])
+y_pred=regressor.predict([x_in])
+
+print('prediction=',y_pred)
 
 from six import StringIO
-from IPython.display import Image
 from sklearn.tree import export_graphviz
 import pydotplus
+from IPython.display import Image
 
 dot_data=StringIO()
 
